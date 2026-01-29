8ad3dbee1fa7d4cd9693482bc95a17f7d32f36af
cem-akkaya <66216177+cem-akkaya@users.noreply.github.com>
2026-01-29 10:39:13 +0300

    Make a kinematic ball as practice

---
+++ Source/Sandbox/Ball.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ball.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+ABall::ABall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>("CollisionSphere");
+	CollisionSphere->InitSphereRadius(50.0f);
+	CollisionSphere->SetCollisionProfileName("BlockAll");
+	CollisionSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionSphere->SetHiddenInGame(false);
+	CollisionSphere->SetLineThickness(2.f);
+	CollisionSphere->SetSimulatePhysics(false);
+	CollisionSphere->SetNotifyRigidBodyCollision(true);
+}
+
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GetInstigator())
+	{
+		LaunchDirection = GetInstigator()->GetActorForwardVector();
+	}
+	CurrentSpeed = InitialSpeed;
+    CollisionSphere->OnComponentHit.AddDynamic(this, &ABall::OnBallHit);
+}
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (CurrentSpeed > 0.0f)
+	{
+		CurrentSpeed += Acceleration * DeltaTime;
+		AddActorWorldOffset(LaunchDirection * CurrentSpeed * DeltaTime, true, nullptr, ETeleportType::None );
+	}
+}
+
+void ABall::OnBallHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	
+	// Just set direction to normal
+	FVector BallLocation = GetActorLocation();
+	LaunchDirection = Hit.Normal;
+}
+
---
+++ Source/Sandbox/Ball.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "Ball.generated.h"
+
+UCLASS()
+class SANDBOX_API ABall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION()
+	void OnBallHit(UPrimitiveComponent* HitComp, AActor* OtherActor,
+				   UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+				   const FHitResult& Hit);
+
+	UPROPERTY(EditAnywhere)
+	float InitialSpeed = 800.f;
+
+	UPROPERTY(EditAnywhere)
+	float Acceleration = 200.f;
+	
+	UPROPERTY(EditAnywhere)
+	FVector LaunchDirection = FVector(1.f, 0.f, 0.1f);
+
+private:
+	float CurrentSpeed;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* CollisionSphere;
+};
