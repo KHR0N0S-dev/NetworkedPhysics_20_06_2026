b17c19fe5eacbbfdddd9fb9f5d2d5c05415472be
cem-akkaya <66216177+cem-akkaya@users.noreply.github.com>
2026-02-07 16:50:47 +0300

    Add Automation Sandbox solution and update project settings, updated SandboxEditor target to use Unreal 5.8 include order version and BuildSettingsVersion V7.

--- Source/SandboxEditor.Target.cs
+++ Source/SandboxEditor.Target.cs
@@ -8,7 +8,8 @@ public class SandboxEditorTarget : TargetRules
 	public SandboxEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		ExtraModuleNames.Add("Sandbox");
 	}
 }
